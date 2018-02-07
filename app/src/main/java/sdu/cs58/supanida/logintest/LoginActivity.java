06685f58f7c0d7515b7b612a6614cd36e05c397b
supanida <sup.ta1909@gmail.com>
2018-02-07 11:47:40 +0700

    Toast Complete

--- app/src/main/java/sdu/cs58/supanida/logintest/LoginActivity.java
+++ app/src/main/java/sdu/cs58/supanida/logintest/LoginActivity.java
@@ -2,12 +2,35 @@ package sdu.cs58.supanida.logintest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    //Explicit
+    EditText  nameEditText,usernameEditText,passwordEditText;
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    }
-}
+
+        //2. Initial view ผูกตัวแปรบน java ให้รู้จักกับ element บน xml
+        nameEditText = findViewById(R.id.txtName);
+        usernameEditText = findViewById(R.id.txtUsername);
+        passwordEditText = findViewById(R.id.txtPassword);
+        loginButton = findViewById(R.id.btnLogin);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Hello",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }//end method onCreate
+
+}// end class
